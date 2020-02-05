8f9dad32262a251fadeb5d8fdb5a23ed5cccd640
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-05 20:27:34 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -1,14 +1,9 @@
 sudo apt-get -y update
-
 dbpass=$1
-
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.7.29 mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server-5.7.29 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
 apt-get -y install apache2 php mysql-server php-mysql php-gd php-zip php-mbstring php-simplexml
-
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
-
 apachectl restart

2b2e8a0d05261d463207c2a96ef5fa7e7312b3e0
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-11 19:50:03 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -19,20 +19,20 @@ apachectl restart
 
 sudo apt-get -y install composer
 
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)" \
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")" \
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)" 
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" 
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")" 
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ] \
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ] 
 then \
-    >&2 echo 'ERROR: Invalid installer signature' \
-    rm composer-setup.php \
-    exit 1 \
-fi \
+    >&2 echo 'ERROR: Invalid installer signature' 
+    sudo rm composer-setup.php 
+    exit 1 
+fi 
 
-sudo php composer-setup.php --quiet \
+sudo php composer-setup.php --quiet 
 RESULT=$? \
-sudo rm composer-setup.php \
+sudo rm composer-setup.php 
 exit $RESULT
 
 cd /var/www/html

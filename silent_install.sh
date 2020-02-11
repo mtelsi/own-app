1f3690772c9489e6ba7335ba251baa041d1865ef
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-12 01:31:52 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -17,7 +17,6 @@ sudo apt-get -y install php-mysql php-gd php-zip php-mbstring php-xml php-curl p
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
-
 ex /etc/apache2/apache2.conf <<EOEX
   :172 s/AllowOverride None/AllowOverride All
   :x
@@ -26,33 +25,34 @@ EOEX
 #sudo echo 'EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 #php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 #ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
 #if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 #then
 #    >&2 echo 'ERROR: Invalid installer checksum'
 #    rm composer-setup.php
 #    exit 1
 #fi
-
 #php composer-setup.php --quiet
 #RESULT=$?
 #rm composer-setup.php
 #exit $RESULT' > /home/ubuntu/installcomposer.sh
-
 #sudo chmod +x /home/ubuntu/installcomposer.sh
 #sudo /home/ubuntu/installcomposer.sh
+
 cd /var/www/html
+
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
 sudo php -r "if (hash_file('sha384', 'composer-setup.php') === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
 sudo php composer-setup.php
+
 sudo php -r "unlink('composer-setup.php');"
 
 cd /var/www/html
+
 sudo mv composer.phar /./bin/composer
 
-
 free -m
-
 sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 #sudo /sbin/mkswap /var/swap.1
 #sudo /sbin/swapon /var/swap.1
@@ -60,14 +60,19 @@ sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 #cd /tmp
 #curl -sS https://getcomposer.org/installer | php
 #sudo mv composer.phar /./bin/composer
+
 cd /var/www/html
 
 sudo composer global require laravel/installer
+
 sudo composer create-project --prefer-dist laravel/laravel blog
+
 sudo chgrp -R www-data /var/www/html/blog
+
 sudo chmod -R 775 /var/www/html/blog/storage
 
 sudo systemctl restart apache2
+
 apachectl restart
 
 

00b50833fe6b498cf7a76b186de8cbcade57df4e
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-11 17:22:26 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -3,7 +3,8 @@ dbpass=$1
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.7.29 mysql-server/root_password password $dbpass | debconf-set-selections
 echo mysql-server-5.7.29 mysql-server/root_password_again password $dbpass | debconf-set-selections
-apt-get -y install apache2 php mysql-server php-mysql php-gd php-zip php-mbstring php-simplexml php-curl php-xml php-xmlrpc php-intl
+apt-get -y install apache2 php mysql-server php-mysql 
+apt-get -y install php-gd php-zip php-mbstring php-simplexml php-curl php-xml php-xmlrpc php-intl
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 

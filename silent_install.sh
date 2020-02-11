415a7a177476c3360a1b305c736de53ac1f53e28
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-11 21:50:27 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -13,7 +13,7 @@ ex /etc/apache2/apache2.conf <<EOEX
 EOEX
 
 sudo echo 'EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)" 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" 
+php -d memory_limit=-1 `which composer` update -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")" 
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ] 

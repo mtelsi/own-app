a139210e74abe4058815349fa9fdbad31663971a
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-12 03:21:34 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -71,11 +71,13 @@ cd /var/www/html
 
 sudo composer global require laravel/installer
 
-sudo composer create-project --prefer-dist laravel/laravel blog
+sudo ~/.composer/vendor/bin/laravel new blog
 
-sudo chgrp -R www-data /var/www/html/blog
+#sudo composer create-project --prefer-dist laravel/laravel blog
 
-sudo chmod -R 775 /var/www/html/blog/storage
+#sudo chgrp -R www-data /var/www/html/blog
+
+#sudo chmod -R 775 /var/www/html/blog/storage
 
 sudo systemctl restart apache2
 

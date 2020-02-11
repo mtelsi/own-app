5cec616e199f1ed35316ffaad753c8e44cad470c
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-11 23:23:15 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -1,16 +1,18 @@
+
 sudo apt-get -y update
 dbpass=$1
-# export DEBIAN_FRONTEND=noninteractive
-# echo mysql-server-5.7.29 mysql-server/root_password password $dbpass | debconf-set-selections
-# echo mysql-server-5.7.29 mysql-server/root_password_again password $dbpass | debconf-set-selections
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.7.29 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.7.29 mysql-server/root_password_again password $dbpass | debconf-set-selections
 sudo apt-get -y install apache2 php mysql-server
-sudo apachectl restart
 sudo apt-get -y install php-mysql php-gd php-zip php-mbstring php-simplexml php-curl php-xml php-xmlrpc php-intl
 
 ex /etc/apache2/apache2.conf <<EOEX
   :172 s/AllowOverride None/AllowOverride All
   :x
 EOEX
+
+apachectl restart
 
 sudo echo 'EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -25,7 +27,7 @@ fi
 
 php composer-setup.php --quiet
 RESULT=$?
-sudo rm composer-setup.php
+rm composer-setup.php
 exit $RESULT' > /home/ubuntu/installcomposer.sh
 
 sudo chmod +x /home/ubuntu/installcomposer.sh

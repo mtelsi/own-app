2de11c918532285b6faac12ef40437969644da38
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-11 21:58:19 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -12,14 +12,14 @@ ex /etc/apache2/apache2.conf <<EOEX
   :x
 EOEX
 
-sudo echo 'EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)" 
-php -d memory_limit=-1 `which composer` update -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")" 
+sudo echo 'EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ] 
-then 
-    >&2 echo 'ERROR: Invalid installer signature' 
-    sudo rm composer-setup.php 
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
     exit 1
 fi
 

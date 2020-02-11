1e864b9df922a03fb4668a5ed821325a9619e3fa
mtelsi <60696559+mtelsi@users.noreply.github.com>
2020-02-11 16:27:31 +0800

    Update silent_install.sh

--- silent_install.sh
+++ silent_install.sh
@@ -6,12 +6,13 @@ echo mysql-server-5.7.29 mysql-server/root_password_again password $dbpass | deb
 apt-get -y install apache2 php mysql-server php-mysql php-gd php-zip php-mbstring php-simplexml php-curl php-xml php-xmlrpc php-intl
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
-apachectl restart
 
 ex /etc/apache2/apache2.conf <<EOEX
   :172 s/AllowOverride None/AllowOverride All
   :x
 EOEX
+
+apachectl restart
 
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
